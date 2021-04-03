292c6f06b0f445519d7a3d3dca9ca4921be5ae33
abasery1997 <abdoumustafa1997@gmail.com>
2021-04-03 17:44:17 +0300

    task2

--- task2/pages/main.js
+++ task2/pages/main.js
@@ -1,15 +1,17 @@
 const input_form = document.querySelector('.new-todo-form')
 const newTodo = document.querySelector('.new-todo')
 const container = document.querySelector('.todos-container')
-
+let fetchedTodoNum = 10;
 
 
 // create li with the title of the todo
-const createTodo = (value) => {
+const createTodo = (value, id, completed) => {
     const todo = document.createElement('li')
     todo.textContent = value;
     const delbtn = document.createElement('button')
     delbtn.textContent = 'finished';
+    todo.id = id;
+    todo.setAttribute("completed", completed);
     todo.appendChild(delbtn)
     container.appendChild(todo)
     delbtn.addEventListener('click', () => {
@@ -24,13 +26,22 @@ const createTodo = (value) => {
 fetch("http://127.0.0.1:3300/fetched")
     .then((response) => response.json())
     .then((todos) => {
-        for (let i = 0; i < 10; i++) {
-            createTodo(todos[i].title);
+        for (let i = 0; i < fetchedTodoNum; i++) {
+            createTodo(todos[i].title, todos[i].id, todos[i].completed);
         }
     })
-
+let id = fetchedTodoNum;
 //add new todo
 input_form.addEventListener('submit', (e) => {
     e.preventDefault();
-    createTodo(newTodo.value);
+    //  const id = Date.now();
+    const completed = Math.random() < 0.5;
+    if (newTodo.value.trim()) {
+        id++;
+        createTodo(newTodo.value, id, completed);
+        newTodo.value = ''
+    } else {
+        alert("enter the todo title ")
+    }
+
 })
